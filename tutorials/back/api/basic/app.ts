13ee9cbc8eb160846e31bd5cd717b28cf05381e8
Moaze <moaze.mobarak@student.vinci.be>
2025-09-24 15:15:33 +0200

    new: ex1.2

--- tutorials/back/api/basic/app.ts
+++ tutorials/back/api/basic/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 import usersRouter from "./routes/users";
 import pizzaRouter from "./routes/pizzas";
 import drinkRouter from "./routes/drinks"
+import { ErrorRequestHandler } from "express";
 
 const app = express();
 
@@ -12,6 +13,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/users", usersRouter);
 app.use("/pizzas", pizzaRouter);
 app.use("/drinks", drinkRouter);
+
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+  console.error(err.stack);
+  return res.status(500).send("Something broke!");
+};
+
+app.use(errorHandler);
+
 
 export default app;
 
